9d153ee7f7875277a95bbf6f420e58288266b225
daocheng815 <c123456ron@gmail.com>
2023-10-24 23:47:33 +0800

    0.0.4.8 commit

--- copyUnity.bat
+++ copyUnity.bat
@@ -2,12 +2,15 @@
 set /p choice="Do you want to copy the folder? (Y/N): "
 
 if /i "%choice%"=="Y" (
-    xcopy "E:\gameproject\2D ACT\Assets\Scripts" "C:\Git\Unity\Scripts" /E /Y
-    xcopy "E:\gameproject\2D ACT\Assets\Resources" "C:\Git\Unity\Resources" /E /Y
-    xcopy "E:\gameproject\2D ACT\Assets\Inventory" "C:\Git\Unity\Inventory" /E /Y
-    echo Copy completed.
+	del /s /q "C:\Git\Unity\Inventory\*.*"
+	del /s /q "C:\Git\Unity\Resources\*.*"
+	del /s /q "C:\Git\Unity\Scripts\*.*"
+	xcopy "E:\gameproject\2D ACT\Assets\Scripts" "C:\Git\Unity\Scripts" /E /Y
+	xcopy "E:\gameproject\2D ACT\Assets\Resources" "C:\Git\Unity\Resources" /E /Y
+	xcopy "E:\gameproject\2D ACT\Assets\Inventory" "C:\Git\Unity\Inventory" /E /Y
+	echo Copy completed.
 ) else (
-    echo Copy canceled.
+	echo Copy canceled.
 )
 
 pause
