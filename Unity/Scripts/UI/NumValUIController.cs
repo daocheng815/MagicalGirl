3be78a9a59d4922c529c445c9afbeabe631c6065
daocheng815 <c123456ron@gmail.com>
2023-10-25 12:35:34 +0800

    0.0.5.0 commit

--- Unity/Scripts/UI/NumValUIController.cs
+++ Unity/Scripts/UI/NumValUIController.cs
@@ -5,8 +5,11 @@ using UnityEngine.UI;
 public class NumValUIController : MonoBehaviour
 {
     public PlayerController pc;
+    [SerializeField]private bool isplaymove;
     [SerializeField]private bool timerBool = false;
     [SerializeField]private int timer = 0;
+    private bool isExitUi = false;
+    private bool ison = false;
     public int timerDelay = 5;
     public float waitTime = 1f;
     public float animatorValNum = 100f;
@@ -23,14 +26,19 @@ public class NumValUIController : MonoBehaviour
             rtV3[i] = rtList[i].position;
             rtList[i].position = new Vector3(rtV3[i].x,rtV3[i].y+animatorValNum,rtV3[i].z);
         }
+        //每1秒判定玩家是否正在運動
+        InvokeRepeating("GlobalTimer",0f,0.1f);
+        //延遲一秒再開始所有計時器與UI判定
         Invoke("Fadeamimator",1f);
     }
     public void Fadeamimator()
     {
+        ison = true;
+        isExitUi = false;
+        OnTimer();
         StartCoroutine(NumValAnimator(animatorValNum, waitTime, 0));
         StartCoroutine(NumValAnimator(animatorValNum, waitTime, 1));
         // 計時器
-        OnTimer();
     }
     // 玩家數值條動畫效果
     public IEnumerator NumValAnimator(float val = 50f,float timeDelay = 1f,int NumVal = 0,bool fadeMod = true)
@@ -73,29 +81,72 @@ public class NumValUIController : MonoBehaviour
         timer++;
         //Debug.Log("計時中:" + timer);
     }
-
+    //開啟UI計時器
     void OnTimer()
     {
         timerBool = true;
         InvokeRepeating("AnimatorTimer",0f,1f);
     }
-
+    //關閉UI計時器
     void ExitTimer()
     {
         CancelInvoke("AnimatorTimer");
         timerBool = false;
         timer = 0;
     }
+    // 全域計時器，用來判定玩家是否移動的延遲判定
+    private int GlobalTimerdelay;
+    void GlobalTimer()
+    {
+        //如果玩家移動了就將bool所在鎖在True狀態2S，判定精度是0.1s
+        if (isplaymove && GlobalTimerdelay < 20)
+        {
+            if(pc.rb.velocity.x != 0 || pc.rb.velocity.y != 0 )
+                GlobalTimerdelay = 0;
+            GlobalTimerdelay++;
+            isplaymove = isplaymove;
+        }
+        else
+        {
+            GlobalTimerdelay = 0;
+            isplaymove = (pc.rb.velocity.x != 0 || pc.rb.velocity.y != 0 );
+        }
+    }
     public void Update()
     {
-        
-        if (timer > timerDelay && timerBool)
+        //全域延遲
+        if (ison)
         {
-            ExitTimer();
-        }
-        if (!timerBool)
-        {
-            OnTimer();
+            //如果計時器時間到了，且計時器開啟著就關閉計時器
+            if (timer > timerDelay && timerBool)
+            {
+                ExitTimer();
+            }
+            //時間到了就關閉UI
+            if (!timerBool && !isExitUi)
+            {
+                if (!isplaymove)
+                {
+                    isExitUi = true;
+                    StartCoroutine(NumValAnimator(animatorValNum, waitTime, 0,false));
+                    StartCoroutine(NumValAnimator(animatorValNum, waitTime, 1,false));
+                }
+            }
+            //如果UI已經關閉，且玩家有在移動
+            if (!timerBool && isExitUi && isplaymove)
+            {
+                isExitUi = false;
+                OnTimer();
+                StartCoroutine(NumValAnimator(animatorValNum, waitTime, 0,true));
+                StartCoroutine(NumValAnimator(animatorValNum, waitTime, 1,true));
+            }
+            //如果玩家死亡也要縮上去
+            if (!pc.IsAlive)
+            {
+                isExitUi = true;
+                StartCoroutine(NumValAnimator(animatorValNum, waitTime, 0,false));
+                StartCoroutine(NumValAnimator(animatorValNum, waitTime, 1,false));
+            }
         }
     }
 }
